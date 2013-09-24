62f5a2659e9d17195ee568e11d23b250e30ef41b
Ivan <autopilot_off@mail.ru>
2013-09-24 15:23:44 +0600

    создана инфраструктура с БД
    отображение категорий и товаров, постраничная навигация

---
+++ Store.WebUI/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Store.WebUI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                null,
+                "", // соответствует только пустому URL
+                new { controller = "Product", action = "List" , category = (string)null, page = 1}
+                );
+            routes.MapRoute(
+                null,
+                "Page{page}", // соответствует /Page2, /Page123, но не /PageXYZ
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" } // ограничения: страница должна быть чистой
+                );
+            routes.MapRoute(
+                null,
+                "{category}", // соответствует /Мониторы
+                new { controller = "Product", action = "List",  page = 1 }
+                );
+            routes.MapRoute(
+                null,
+                "{category}/Page{page}", // соответвтвует /Мониторы/Page567
+                new { controller = "Product", action = "List" }, // по умолчанию
+                new { page = @"\d+" } // ограничения: страница должна быть чистой
+            );
+            routes.MapRoute(null, "{controller}/{action}");
+        }
+    }
+}
---
+++ Store.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.Mvc;
+using Store.WebUI.Models;
+
+namespace Store.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString Pagelinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a"); // Создание дескриптора <a>
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pagingInfo.CurrentPage)
+                    tag.AddCssClass("selected");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
